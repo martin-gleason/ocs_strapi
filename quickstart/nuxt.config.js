9635fc29e42542395c81e683afe82c394abdcbac
marty-gleason <officergleason@gmail.com>
2019-07-17 08:14:18 -0500

    move quickstart/rebuild

--- quickstart/nuxt.config.js
+++ quickstart/nuxt.config.js
@@ -3,20 +3,25 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'ocs_strapi',
+    title: 'strapi_quickstart',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'nuxt JS/Vue components ' }
+      { hid: 'description', name: 'description', content: 'test of nuxt for ocs_intranet' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
+
+
+  modules: [
+    'bootstrap-vue/nuxt'
+  ],
   /*
   ** Customize the progress bar color
   */
-  loading: { color: '#0A2463' },
+  loading: { color: '#3B8070' },
   /*
   ** Build configuration
   */
@@ -34,9 +39,6 @@ module.exports = {
         })
       }
     }
-    modules: [
-      'bootstrap-vue/nuxt',
-    ]
   }
 }
 
